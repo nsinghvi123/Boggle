ec01aaabc500b298ff0e8715e2d4201e876cd9e4
Natasha Singhvi <nsinghvi@umich.edu>
2020-07-21 15:49:43 -0400

    created constructor and added private member variable in full word constructor

--- boggle-lib/src/main/java/com/boggle/FullWordCalculator.java
+++ boggle-lib/src/main/java/com/boggle/FullWordCalculator.java
@@ -1,44 +1,22 @@
 package com.boggle;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class FullWordCalculator {
+    private Set<String> dictionary = new HashSet<>();
 
-    public static List<String> readFileInList(String fileName) {
-        List<String> lines = Collections.emptyList();
-        try {
-            lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
-        return lines;
+    public FullWordCalculator(String fileName) throws IOException {
+        List<String> allWords = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+        this.dictionary = new HashSet<>(allWords);
     }
 
-    public static void main(String[] args) {
-        List<String> words = readFileInList("/Users/natashasinghvi/Documents/boggle/boggle-lib/src/main/java/com/boggle/popularWords.txt");
-        Boolean answer = checkIsWordFull("djlk");
-        System.out.println(answer);
-
-    }
-
-    public static Boolean checkIsWordFull(String word) {
-
-        List<String> words = readFileInList("/Users/natashasinghvi/Documents/boggle/boggle-lib/src/main/java/com/boggle/popularWords.txt");
-        HashSet<String> listOfWordsSet = new HashSet<>();
-        listOfWordsSet.addAll(words);
-
-        if (listOfWordsSet.contains(word)){
-            return true;
-        }
-        else{
-            return false;
-        }
+    public Boolean checkIsWordFull(String word) {
+        return dictionary.contains(word);
     }
 }
